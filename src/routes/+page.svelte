c68afaa15b85c2ea2fe5c53e19d04774227ecfac
Christian Hofmeister <christian.hofmeister@mtc.berlin>
2023-11-29 16:41:59 +0100

    demo cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,40 +26,57 @@
 	];
 </script>
 
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<div class="mt-4 max-w-3xl space-y-6">
+    <div class="grid grid-cols-2 gap-60">
+        <Card.Root class="w-[350px]">
+            <Card.Header>
+                <Card.Title>fast</Card.Title>
+                <Card.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                Powered by Svelte and Vite, speed is baked into every crevice: fast setup, fast dev, fast builds, fast page loads, fast navigation. Did we mention it's fast?
+            </Card.Content>
+            <Card.Footer class="flex justify-between">
+                <a href="https://kit.svelte.dev/docs/introduction" class="group flex w-full items-center rounded-md border border-transparent px-2 py-1 hover:underline text-muted-foreground">svelte.de</a>
+            </Card.Footer>
+        </Card.Root>
 
-<Card.Root class="w-[350px]">
-	<Card.Header>
-		<Card.Title>Create project</Card.Title>
-		<Card.Description>Deploy your new project in one-click.</Card.Description>
-	</Card.Header>
-	<Card.Content>
-		<form>
-			<div class="grid w-full items-center gap-4">
-				<div class="flex flex-col space-y-1.5">
-					<Label for="name">Name</Label>
-					<Input id="name" placeholder="Name of your project" />
-				</div>
-				<div class="flex flex-col space-y-1.5">
-					<Label for="framework">Framework</Label>
-					<Select.Root>
-						<Select.Trigger id="framework">
-							<Select.Value placeholder="Select" />
-						</Select.Trigger>
-						<Select.Content>
-							{#each frameworks as framework}
-								<Select.Item value={framework.value} label={framework.label}
-									>{framework.label}</Select.Item
-								>
-							{/each}
-						</Select.Content>
-					</Select.Root>
-				</div>
-			</div>
-		</form>
-	</Card.Content>
-	<Card.Footer class="flex justify-between">
-		<Button variant="outline">Cancel</Button>
-		<Button>Deploy</Button>
-	</Card.Footer>
-</Card.Root>
+        <Card.Root class="w-[350px]">
+            <Card.Header>
+                <Card.Title>Lorem ipsum dolor sit amet</Card.Title>
+                <Card.Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <form>
+                    <div class="grid w-full items-center gap-4">
+                        <div class="flex flex-col space-y-1.5">
+                            <Label for="name">Name</Label>
+                            <Input id="name" placeholder="Name of your project" />
+                        </div>
+                        <div class="flex flex-col space-y-1.5">
+                            <Label for="framework">Framework</Label>
+                            <Select.Root>
+                                <Select.Trigger id="framework">
+                                    <Select.Value placeholder="Select" />
+                                </Select.Trigger>
+                                <Select.Content>
+                                    {#each frameworks as framework}
+                                        <Select.Item value={framework.value} label={framework.label}
+                                            >{framework.label}</Select.Item
+                                        >
+                                    {/each}
+                                </Select.Content>
+                            </Select.Root>
+                        </div>
+                    </div>
+                </form>
+            </Card.Content>
+            <Card.Footer class="flex justify-between">
+                <Button variant="outline">Cancel</Button>
+                <Button>Create</Button>
+            </Card.Footer>
+        </Card.Root>
+        
+    </div>
+</div>
+
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,13 +2,96 @@
 	import { page } from '$app/stores';
 </script>
 
-<header>
-	<nav>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-		</ul>
+<header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm">
+	<nav
+		class="relative max-w-[85rem] w-full mx-auto bg-white border-b border-gray-200 py-3 px-4 sm:py-0 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8 xl:border-x dark:bg-gray-800 dark:border-gray-700"
+		aria-label="Global"
+	>
+		<div class="flex items-center justify-between">
+			<a class="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand"
+				>SvelteKit Test & Auth Demonstration</a
+			>
+			<div class="sm:hidden">
+				<button
+					type="button"
+					class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-lg border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+					data-hs-collapse="#navbar-collapse-with-animation"
+					aria-controls="navbar-collapse-with-animation"
+					aria-label="Toggle navigation"
+				>
+					<svg
+						class="hs-collapse-open:hidden flex-shrink-0 w-4 h-4"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line
+							x1="3"
+							x2="21"
+							y1="18"
+							y2="18"
+						/></svg
+					>
+					<svg
+						class="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4"
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+					>
+				</button>
+			</div>
+		</div>
+		<div
+			id="navbar-collapse-with-animation"
+			class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
+		>
+			<div
+				class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:ps-7"
+			>
+				<a
+					class="font-medium text-blue-600 sm:py-6 dark:text-blue-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+					href="#"
+					aria-current="page">Landing</a
+				>
+				<a
+					class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+					href="#">Account</a
+				>
+				<a
+					class="font-medium text-gray-500 hover:text-gray-400 sm:py-6 dark:text-gray-400 dark:hover:text-gray-500 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+					href="#">Work</a
+				>
+				
+				<a
+					class="flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 sm:border-s sm:border-gray-300 sm:my-6 sm:ps-6 dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500"
+					href="#"
+				>
+					<svg
+						class="flex-shrink-0 w-4 h-4"
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						viewBox="0 0 16 16"
+					>
+						<path
+							d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"
+						/>
+					</svg>
+					Log in
+				</a>
+			</div>
+		</div>
 	</nav>
 </header>
-
